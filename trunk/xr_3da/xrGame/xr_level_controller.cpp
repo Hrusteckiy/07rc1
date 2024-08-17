3529f836892e2ad10163b891373b57e8dd9b7436
Hrusteckiy <kostia4554t@yandex.ru>
2024-08-17 07:48:01 +0300

    Improved log about incorrect action name

--- trunk/xr_3da/xrGame/xr_level_controller.cpp
+++ trunk/xr_3da/xrGame/xr_level_controller.cpp
@@ -247,7 +247,7 @@ _action* action_name_to_ptr(LPCSTR _name)
 			return &actions[idx];
 		++idx;
 	}
-	Msg				("! cant find corresponding [id] for action_name", _name);
+	Msg				("! cant find corresponding [%s] for [%s]", actions[idx].action_name, _name);
 	return			NULL;
 }
 
