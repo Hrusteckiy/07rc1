cfd0cf679bf7d5a6f7e5275b2153f8831e540df5
Hrusteckiy <kostia4554t@yandex.ru>
2024-09-25 14:23:05 +0300

    Fix for rare crash when NPC was dead but weapon was not detached
    ```sv ownership id_parent [24331][NOTFOUND] id_entity [16569][wpn_groza:wpn_groza16569] [23394]```

--- trunk/xr_3da/xrGame/xrServer_process_event_ownership.cpp
+++ trunk/xr_3da/xrGame/xrServer_process_event_ownership.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "xrserver.h"
 #include "xrserver_objects.h"
+#include "xrserver_objects_alife_monsters.h"
+#include "xrServer_svclient_validation.h"
 
 void ReplaceOwnershipHeader	(NET_Packet& P)
 {
@@ -17,22 +19,41 @@ void xrServer::Process_event_ownership(NET_Packet& P, ClientID sender, u32 time,
 	P.r_u16				(id_entity);
 	CSE_Abstract*		e_parent	= game->get_entity_from_eid	(id_parent);
 	CSE_Abstract*		e_entity	= game->get_entity_from_eid	(id_entity);
-
-	#ifdef DEBUG
-	Msg("sv ownership id_parent %s id_entity %s [%d]",ent_name_safe(id_parent).c_str(), ent_name_safe(id_entity).c_str(), Device.dwFrame);
-	#endif
-
-	if(!e_entity)		return;
-	R_ASSERT			(/*e_entity &&*/ e_parent);
-	if (0xffff != e_entity->ID_Parent)
+	
+	
+#ifdef MP_LOGGING
+	Msg( "--- SV: Process ownership take: parent [%d][%s], item [%d][%s]", 
+		id_parent, e_parent ? e_parent->name_replace() : "null_parent",
+		id_entity, e_entity ? e_entity->name() : "null_entity");
+#endif // MP_LOGGING
+	
+	if ( !e_parent ) {
+		Msg( "! ERROR on ownership: parent not found. parent_id = [%d], entity_id = [%d], frame = [%d].", id_parent, id_entity, Device.dwFrame );
+		return;
+	}
+	if ( !e_entity ) {
+//		Msg( "! ERROR on ownership: entity not found. parent_id = [%d], entity_id = [%d], frame = [%d].", id_parent, id_entity, Device.dwFrame );
+		return;
+	}
+	
+	if (!is_object_valid_on_svclient(id_parent))
 	{
-		Msg("sv !ownership (entity already has parent) new_parent %s id_parent %s id_entity %s [%d]",ent_name_safe(e_entity->ID_Parent).c_str(), ent_name_safe(id_parent).c_str(), ent_name_safe(id_entity).c_str(), Device.dwFrame);
+		Msg( "! ERROR on ownership: parent object is not valid on sv client. parent_id = [%d], entity_id = [%d], frame = [%d]", id_parent, id_entity, Device.dwFrame );
 		return;
 	}
 
+	if (!is_object_valid_on_svclient(id_entity))
+	{
+		Msg( "! ERROR on ownership: entity object is not valid on sv client. parent_id = [%d], entity_id = [%d], frame = [%d]", id_parent, id_entity, Device.dwFrame );
+		return;
+	}
+
+	if (0xffff != e_entity->ID_Parent)
+		return;
+
 	xrClientData*		c_parent		= e_parent->owner;
 	xrClientData*		c_entity		= e_entity->owner;
-	xrClientData*		c_from			= ID_to_client	(sender);
+	xrClientData*		c_from			= ID_to_client(sender);
 
 	if ( (GetServerClient() != c_from) && (c_parent != c_from) )
 	{
@@ -40,10 +61,18 @@ void xrServer::Process_event_ownership(NET_Packet& P, ClientID sender, u32 time,
 		return;
 	}
 
+	CSE_ALifeCreatureAbstract* alife_entity = smart_cast<CSE_ALifeCreatureAbstract*>(e_parent);
+	if (alife_entity && !alife_entity->g_Alive() && game->Type() != GAME_SINGLE)
+	{
+#ifdef MP_LOGGING
+		Msg("--- SV: WARNING: dead player [%d] tries to take item [%d]", id_parent, id_entity);
+#endif //#ifdef MP_LOGGING
+		return;
+	};
+
 	// Game allows ownership of entity
 	if (game->OnTouch	(id_parent,id_entity, bForced))
 	{
-
 		// Perform migration if needed
 		if (c_parent != c_entity)		PerformMigration(e_entity,c_entity,c_parent);
 
